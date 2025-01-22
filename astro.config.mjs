354c663d890eccfae947ec39bd2457ce8197da02
austinleblanc <79474577+austinleblanc@users.noreply.github.com>
2025-01-22 16:00:55 -0500

    pushing workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nationaloralsexday.com',
+    site: 'https://nationaloralsexday.org',
 });
